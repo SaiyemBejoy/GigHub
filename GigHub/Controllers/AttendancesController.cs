ffcb7e06d00ab895a455f9e4643756eade7cc28b
SaiyemBejoy <jsbejoy96@gmail.com>
2020-09-13 17:49:11 +0600

    Added AttendanceDton and funtionalities for Going Button for Gig

--- GigHub/Controllers/AttendancesController.cs
+++ GigHub/Controllers/AttendancesController.cs
@@ -1,4 +1,5 @@
-﻿using GigHub.Models;
+﻿using GigHub.Dtos;
+using GigHub.Models;
 using Microsoft.AspNet.Identity;
 using System;
 using System.Collections.Generic;
@@ -9,6 +10,7 @@ using System.Web.Http;
 
 namespace GigHub.Controllers
 {
+
     [Authorize]
     public class AttendancesController : ApiController
     {
@@ -20,12 +22,19 @@ namespace GigHub.Controllers
         }
 
         [HttpPost]
-        public IHttpActionResult Attend([FromBody] int gigId)
+        public IHttpActionResult Attend(AttendanceDto dto)
         {
+            var userId = User.Identity.GetUserId();
+            //var exists = _context.Attendances.Any(a => a.AttendeeId == userId && a.GigId == gigId);
+
+            if (_context.Attendances.Any(a => a.AttendeeId == userId && a.GigId == dto.GigId))
+                return BadRequest("The attendance already exists.");
+
+
             var attendance = new Attendance
             {
-                GigId = gigId,
-                AttendeeId = User.Identity.GetUserId()
+                GigId = dto.GigId,
+                AttendeeId = userId
             };
             _context.Attendances.Add(attendance);
             _context.SaveChanges();
